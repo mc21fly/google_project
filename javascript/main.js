7b5eceb24114509eba3d91dc19565e6fa5c43ebb
Michał <michalbkl@poczta.fm>
2019-08-31 11:53:43 +0200

    update

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const searchField = document.getElementById("search-field");
+const searchBar = document.getElementById("search-bar");
+
+searchField.addEventListener("focusin", function() {
+    searchBar.style.boxShadow = "0 0 5px rgba(128, 128, 128, 0.363)";
+})
+
+searchField.addEventListener("focusout", function() {
+    searchBar.style.boxShadow = "";
+})
